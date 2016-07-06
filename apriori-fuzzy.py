e8e503643535b75a243bbfe7913f6bbee507ebe9
Kritika Nangia <kritikanangia@yahoo.co.in>
2016-07-06 15:36:11 +0530

    changed everything to sets.fuzzy arm almost complete.
    Now the joinset and confidence from the past implementation can be used with our support function.Added comments.

--- apriori-fuzzy.py
+++ apriori-fuzzy.py
@@ -31,31 +31,39 @@ def calSupport(itemSet):
 
 
 def returnItemsWithMinSupport(subsetList,minSupport):
-    _itemSet = []
+    _itemSet = set()
+    #_itemSet would be a set of sets.It would contain those sets from the subsetList which surpass minsupport.
     for x in subsetList:
         support = calSupport(x)
         if support >= minSupport:
-            _itemSet.append(x)
+            _itemSet.add(x)
 
     return _itemSet
 
 def joinSet(currentLSet,length):
-    pass
+    """Join a set with itself and returns the n-element itemsets""" 
+    return set([i.union(j) for i in itemSet for j in itemSet if len(i.union(j)) == length]) 
+
 
 
 def runApriori(minSupport,num):
 
-    l = []
+    l = set()
+    #l is a set of sets. It contains sets of individiual attributes({1},{2},{3},etc.).C1 accd.to apriori.
     
     for i in range(1,num):
-        l.append([i])
+        l.add(set([i]))
+    
+    largeSet=dict()
+   # largeSet is a dictionary to store all frequent itemsets, later on used to calculate rules.
     
     oneCSet = returnItemsWithMinSupport(l,minSupport)
+    # set of sets containing frequent itemsets of length 1. L1 accd. to apiori.
 
     currentLSet = oneCSet
     k = 2
 
-    while(currentLSet != list([])):
+    while(currentLSet != set([])):
         largeSet[k-1] = currentLSet
         currentLSet = joinSet(currentLSet, k)
         currentCSet = returnItemsWithMinSupport(currentLSet,minSupport)
@@ -63,7 +71,23 @@ def runApriori(minSupport,num):
         k = k + 1
 
 
-    #MAKE JOINSET function
+    
     # Furthur work on Calculating Confidence
+    toRetItems = []
+    for key, value in largeSet.items():
+        toRetItems.extend([(tuple(item), calSupport(item))
+                           for item in value])
 
+    toRetRules = []
+    for key, value in largeSet.items()[1:]:
+        for item in value:
+            _subsets = map(frozenset, [x for x in subsets(item)])
+            for element in _subsets:
+                remain = item.difference(element) 
+                if len(remain) > 0:  #change condition to len(remain)=0 if we want only single element on right side rules.or change remain=num if we want for lst element only.)
+                    confidence = calSupport(item)/calSupport(element)
+                    if confidence >= minConfidence:
+                        toRetRules.append(((tuple(element), tuple(remain)),
+                                           confidence))
+    return toRetItems, toRetRules
 
